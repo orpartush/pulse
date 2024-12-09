64a97d088909c8c95bca49daad09b92b0021cf72
orpartush <orpt.dev@gmail.com>
2024-12-09 23:14:11 +0200

    feat: iconify-icon and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { supabase } from '@/lib/supabaseClient'
+import Input from './components/ui/input/Input.vue'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import Avatar from './components/ui/avatar/Avatar.vue'
+import AvatarImage from './components/ui/avatar/AvatarImage.vue'
+import AvatarFallback from './components/ui/avatar/AvatarFallback.vue'
+
 console.log(supabase)
 </script>
 
 <template>
-  <main>
-    <RouterView></RouterView>
-  </main>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
+    <form class="relative w-full max-w-96 h-fit">
+      <iconify-icon
+        class="absolute top-[50%] translate-y-[-50%] left-2.5 text-muted-foreground"
+        icon="lucide:search"
+      ></iconify-icon>
+      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </nav>
+  <RouterView></RouterView>
 </template>
 
 <style scoped></style>
